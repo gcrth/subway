b983353843051ddcfa2de74359350e89376e9c3f
昊 张 <gcrth@outlook.com>
2019-01-12 22:14:36 +0800

    BIN

---
+++ BIN/gui.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import turtle
+f = open("result.txt", "r")
+str=f.read()
+f.close()
+route=str.split('\n')
+f=open("beijing_position.txt","r")
+str=f.read()
+f.close()
+station=str.split('\n')
+station_position={}
+for i in range(len(station)-1):
+	str=station[i].split('\t')
+	station_position[str[0]]={'x':int(str[1]),'y':int(str[2])}
+
+
+turtle.screensize(945,709, "white")
+turtle.bgpic(r'subway_map.png')
+
+turtle.color("red", "yellow")
+turtle.speed('slowest')
+# turtle.hideturtle()
+
+turtle.penup()
+turtle.goto(station_position[route[0]]['x']-453,-station_position[route[0]]['y']+340)
+turtle.pendown()
+turtle.dot(10)
+
+for i in range(len(route)-1):
+	turtle.goto(station_position[route[i]]['x']-453,-station_position[route[i]]['y']+340)
+	turtle.dot(10)
+
+turtle.mainloop()
